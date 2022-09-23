a11e5e693b9737ed4c28ec4f0aa41c253b4f95ad
Matteo Giusti <suikofire@gmail.com>
2022-09-23 18:37:43 +0200

    Adds express

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const blasphemyGenerator = require('./blasphemy-generator');
 
-console.log(blasphemyGenerator.generate());
-console.log(blasphemyGenerator.generate());
-console.log(blasphemyGenerator.generate());
-console.log(blasphemyGenerator.generate());
-console.log(blasphemyGenerator.generate());
-console.log(blasphemyGenerator.generate());
-console.log(blasphemyGenerator.generate());
-console.log(blasphemyGenerator.generate());
-console.log(blasphemyGenerator.generate());
+const express = require('express');
+const app = express();
+const defaultPort = 3000;
+
+app.get('/blasphemy', (req, res) => res.json({
+    blasphemy : blasphemyGenerator.generate(),
+    ts : Date.now()
+}));
+
+const port = process.env.PORT || defaultPort;
+app.listen(port, () => {
+    console.log(`Started listening on port ${port}`)
+});
