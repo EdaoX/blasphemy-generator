95eba0c78192f183f95c83ce5d526cdcc98c8c08
Matteo Giusti <suikofire@gmail.com>
2022-09-24 11:46:44 +0200

    Adds data

--- data/adjectives.js
+++ data/adjectives.js
@@ -1,13 +1,15 @@
 const male = [
     'bastardo', 'lurido', 'infame', 'fatiscente', 'rognoso', 'scontato', 'spavaldo', 'sfarzoso', 'lascivo',
     'dispettoso', 'combinaguai', 'insolvente', 'schizofrenico', 'cotto a vapore', 'spiritoso', 'bestia', 'di seconda mano',
-    'iracondo', 'sospettoso', 'misantropo', 'scorretto', 'squallido', 'rompicoglioni', 'urlatore', 'da due soldi', 'nero'
+    'iracondo', 'sospettoso', 'misantropo', 'scorretto', 'squallido', 'rompicoglioni', 'urlatore', 'da due soldi', 'nero', 
+    'dal carattere di merda'
 ];
 
 const female = [
     'bastarda', 'lurida', 'infame', 'fatiscente', 'rognosa', 'scontata', 'spavalda', 'sfarzosa', 'lasciva',
     'dispettosa', 'combinaguai', 'insolvente', 'schizofrenica', 'cotta a vapore', 'spiritosa', 'bestia', 'di seconda mano',
-    'iraconda', 'sospettosa', 'misantropa', 'scorretta', 'squallida', 'rompicoglioni', 'urlatrice', 'da due soldi', 'nera'
+    'iraconda', 'sospettosa', 'misantropa', 'scorretta', 'squallida', 'rompicoglioni', 'urlatrice', 'da due soldi', 'nera', 
+    'dal carattere di merda'
 ];
 
 module.exports = { male, female };
--- data/jobs.js
+++ data/jobs.js
@@ -1,11 +1,15 @@
+const t = require('./tokens');
+const w = t.wrap;
+const c = t.choose;
+
 const male = [
-    'pornodivo', 'giardiniere', 'puttaniero', 'allevatore di maiali', 'stagista', 'corriere Amazon', 'becchino', 'assassino', 'schiavista', 'idraulico',
-    'spacciatore', 'pescatore', 'merdaiolo', 'proctologo'
+    'pornodivo', 'giardiniere', 'puttaniero', `allevatore di ${w(c(t.MALE_ANIMALS_PLURAL, t.FEMALE_ANIMALS_PLURAL))}`, 'stagista', 'corriere Amazon', 'becchino', 'assassino', 'schiavista', 'idraulico',
+    'spacciatore', 'pescatore', 'merdaiolo', 'proctologo', `venditore di ${w(c(t.MALE_ITEMS_PLURAL, t.FEMALE_ITEMS_PLURAL, t.MALE_ANIMALS_PLURAL, t.FEMALE_ANIMALS_PLURAL))}`
 ];
 
 const female = [
-    'pornoattrice', 'meretrice', 'allevatrice di maiali', 'stagista', 'corriere Amazon', 'becchina', 'assassina', 'schiavista', 'idraulica',
-    'spacciatrice', 'pescatrice', 'merdaiola', 'proctologa'
+    'pornoattrice', 'meretrice', `allevatrice di ${w(c(t.MALE_ANIMALS_PLURAL, t.FEMALE_ANIMALS_PLURAL))}`, 'stagista', 'corriere Amazon', 'becchina', 'assassina', 'schiavista', 'idraulica',
+    'spacciatrice', 'pescatrice', 'merdaiola', 'proctologa', `venditrice di ${w(c(t.MALE_ITEMS_PLURAL, t.FEMALE_ITEMS_PLURAL, t.MALE_ANIMALS_PLURAL, t.FEMALE_ANIMALS_PLURAL))}`
 ];
 
 module.exports = { male, female };
