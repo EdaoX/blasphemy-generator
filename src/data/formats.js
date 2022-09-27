4787df30c4fda5fce61f23c2779f053c2eaec6ea
Matteo Giusti <suikofire@gmail.com>
2022-09-27 18:28:30 +0200

    Fixes formats

--- src/data/formats.js
+++ src/data/formats.js
@@ -3,34 +3,36 @@ const w = t.wrap;
 const o = t.optional;
 const c = t.choose;
 
+const composedMaleDivinity = `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ADJECTIVES, t.MALE_ANIMALS, t.MALE_ITEMS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)}`;
+const composedFemaleDivinity = `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ADJECTIVES, t.FEMALE_ANIMALS, t.FEMALE_ITEMS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)}`
+
 module.exports = [
-    `${w(c(t.MALE_ANIMALS, t.FEMALE_ANIMALS, t.MALE_ITEMS, t.FEMALE_ITEMS))} di ${w(c(t.MALE_DIVINITIES, t.FEMALE_NO_MADONNA_DIVINITIES))}`,
-    `${w(c(t.MALE_ANIMALS, t.FEMALE_ANIMALS, t.MALE_ITEMS, t.FEMALE_ITEMS))} di ${w(c(t.MALE_DIVINITIES, t.FEMALE_NO_MADONNA_DIVINITIES))}`,
+    `${w(c(t.MALE_ANIMALS, t.FEMALE_ANIMALS, t.MALE_ITEMS, t.FEMALE_ITEMS))} di ${w(c(t.MALE_DIVINITIES))} ${o(c(t.MALE_ADJECTIVES, t.MALE_JOBS))}`,
+    `${w(c(t.MALE_ANIMALS, t.FEMALE_ANIMALS, t.MALE_ITEMS, t.FEMALE_ITEMS))} di ${w(c(t.FEMALE_NO_MADONNA_DIVINITIES))} ${o(c(t.FEMALE_ADJECTIVES, t.FEMALE_JOBS))}`,
     `${w(c(t.MALE_ANIMALS, t.FEMALE_ANIMALS, t.MALE_ITEMS, t.FEMALE_ITEMS))} della Madonna`,
 
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ADJECTIVES, t.MALE_ANIMALS, t.MALE_ITEMS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ADJECTIVES, t.FEMALE_ANIMALS, t.FEMALE_ITEMS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)}`,
+    `${composedMaleDivinity}`,
+    `${composedFemaleDivinity}`,
 
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ADJECTIVES, t.MALE_ANIMALS, t.MALE_ITEMS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)} con il ${w(c(t.MALE_ITEMS, t.MALE_ANIMALS))} ${o(t.MALE_ADJECTIVES)} ${w(t.BODY_LOCATIONS)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ADJECTIVES, t.FEMALE_ANIMALS, t.FEMALE_ITEMS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)} con il ${w(c(t.MALE_ITEMS, t.MALE_ANIMALS))} ${o(t.MALE_ADJECTIVES)} ${w(t.BODY_LOCATIONS)}`,
-
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ADJECTIVES, t.MALE_ANIMALS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)} con la ${w(c(t.FEMALE_ITEMS, t.FEMALE_ANIMALS))} ${o(t.FEMALE_ADJECTIVES)} ${w(t.BODY_LOCATIONS)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ADJECTIVES, t.FEMALE_ANIMALS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)} con la ${w(c(t.FEMALE_ITEMS, t.FEMALE_ANIMALS))} ${o(t.FEMALE_ADJECTIVES)} ${w(t.BODY_LOCATIONS)}`,
+    `${composedMaleDivinity} con il ${w(c(t.MALE_ITEMS, t.MALE_ANIMALS))} ${o(t.MALE_ADJECTIVES)} ${w(t.BODY_LOCATIONS)}`,
+    `${composedFemaleDivinity} con il ${w(c(t.MALE_ITEMS, t.MALE_ANIMALS))} ${o(t.MALE_ADJECTIVES)} ${w(t.BODY_LOCATIONS)}`,
+    `${composedMaleDivinity} con la ${w(c(t.FEMALE_ITEMS, t.FEMALE_ANIMALS))} ${o(t.FEMALE_ADJECTIVES)} ${w(t.BODY_LOCATIONS)}`,
+    `${composedFemaleDivinity} con la ${w(c(t.FEMALE_ITEMS, t.FEMALE_ANIMALS))} ${o(t.FEMALE_ADJECTIVES)} ${w(t.BODY_LOCATIONS)}`,
     
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ADJECTIVES, t.MALE_ANIMALS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)} che vive ${w(t.LOCATIONS)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ADJECTIVES, t.FEMALE_ANIMALS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)} che vive ${w(t.LOCATIONS)}`,
+    `${composedMaleDivinity} che vive ${w(t.LOCATIONS)}`,
+    `${composedFemaleDivinity} che vive ${w(t.LOCATIONS)}`,
 
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ANIMALS, t.MALE_ITEMS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)} che ${w(t.VERBS)} i ${w(c(t.MALE_ANIMALS_PLURAL, t.MALE_ITEMS_PLURAL))} ${o(t.LOCATIONS)}`,
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ANIMALS, t.MALE_ITEMS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)} che ${w(t.VERBS)} le ${w(c(t.FEMALE_ANIMALS_PLURAL, t.FEMALE_ITEMS_PLURAL))} ${o(t.LOCATIONS)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ANIMALS, t.FEMALE_ITEMS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)} che ${w(t.VERBS)} i ${w(c(t.MALE_ANIMALS_PLURAL, t.MALE_ITEMS_PLURAL))} ${o(t.LOCATIONS)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ANIMALS, t.FEMALE_ITEMS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)} che ${w(t.VERBS)} le ${w(c(t.FEMALE_ANIMALS_PLURAL, t.FEMALE_ITEMS_PLURAL))} ${o(t.LOCATIONS)}`,
+    `${composedMaleDivinity} che ${w(t.VERBS)} i ${w(c(t.MALE_ANIMALS_PLURAL, t.MALE_ITEMS_PLURAL))} ${o(t.LOCATIONS)}`,
+    `${composedMaleDivinity} che ${w(t.VERBS)} le ${w(c(t.FEMALE_ANIMALS_PLURAL, t.FEMALE_ITEMS_PLURAL))} ${o(t.LOCATIONS)}`,
+    `${composedFemaleDivinity} che ${w(t.VERBS)} i ${w(c(t.MALE_ANIMALS_PLURAL, t.MALE_ITEMS_PLURAL))} ${o(t.LOCATIONS)}`,
+    `${composedFemaleDivinity} che ${w(t.VERBS)} le ${w(c(t.FEMALE_ANIMALS_PLURAL, t.FEMALE_ITEMS_PLURAL))} ${o(t.LOCATIONS)}`,
 
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ANIMALS, t.MALE_ITEMS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)} che ${w(t.VERBS_TARGETTABLE)} i ${w(t.MALE_ITEMS_PLURAL)} ai ${w(t.MALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ANIMALS, t.MALE_ITEMS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)} che ${w(t.VERBS_TARGETTABLE)} i ${w(t.MALE_ITEMS_PLURAL)} ai ${w(t.MALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ANIMALS, t.MALE_ITEMS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)} che ${w(t.VERBS_TARGETTABLE)} le ${w(t.FEMALE_ITEMS_PLURAL)} alle ${w(t.FEMALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
-    `${w(t.MALE_DIVINITIES)} ${w(c(t.MALE_ANIMALS, t.MALE_ITEMS, t.MALE_JOBS))} ${o(t.MALE_ADJECTIVES)} che ${w(t.VERBS_TARGETTABLE)} le ${w(t.FEMALE_ITEMS_PLURAL)} alle ${w(t.FEMALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ANIMALS, t.FEMALE_ITEMS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)} che ${w(t.VERBS_TARGETTABLE)} i ${w(t.MALE_ITEMS_PLURAL)} ai ${w(t.MALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ANIMALS, t.FEMALE_ITEMS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)} che ${w(t.VERBS_TARGETTABLE)} i ${w(t.MALE_ITEMS_PLURAL)} ai ${w(t.MALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ANIMALS, t.FEMALE_ITEMS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)} che ${w(t.VERBS_TARGETTABLE)} le ${w(t.FEMALE_ITEMS_PLURAL)} alle ${w(t.FEMALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
-    `${w(t.FEMALE_DIVINITIES)} ${w(c(t.FEMALE_ANIMALS, t.FEMALE_ITEMS, t.FEMALE_JOBS))} ${o(t.FEMALE_ADJECTIVES)} che ${w(t.VERBS_TARGETTABLE)} le ${w(t.FEMALE_ITEMS_PLURAL)} alle ${w(t.FEMALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
+    `${composedMaleDivinity}  che ${w(t.VERBS_TARGETTABLE)} i ${w(t.MALE_ITEMS_PLURAL)} ai ${w(t.MALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
+    `${composedMaleDivinity} che ${w(t.VERBS_TARGETTABLE)} i ${w(t.MALE_ITEMS_PLURAL)} ai ${w(t.MALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
+    `${composedMaleDivinity} che ${w(t.VERBS_TARGETTABLE)} le ${w(t.FEMALE_ITEMS_PLURAL)} alle ${w(t.FEMALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
+    `${composedMaleDivinity} che ${w(t.VERBS_TARGETTABLE)} le ${w(t.FEMALE_ITEMS_PLURAL)} alle ${w(t.FEMALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
+    `${composedFemaleDivinity} che ${w(t.VERBS_TARGETTABLE)} i ${w(t.MALE_ITEMS_PLURAL)} ai ${w(t.MALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
+    `${composedFemaleDivinity} che ${w(t.VERBS_TARGETTABLE)} i ${w(t.MALE_ITEMS_PLURAL)} ai ${w(t.MALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
+    `${composedFemaleDivinity} che ${w(t.VERBS_TARGETTABLE)} le ${w(t.FEMALE_ITEMS_PLURAL)} alle ${w(t.FEMALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
+    `${composedFemaleDivinity} che ${w(t.VERBS_TARGETTABLE)} le ${w(t.FEMALE_ITEMS_PLURAL)} alle ${w(t.FEMALE_ANIMALS_PLURAL)} ${o(t.LOCATIONS)}`,
 ];
